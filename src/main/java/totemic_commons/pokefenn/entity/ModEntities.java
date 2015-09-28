5810fe6ba56ff5e3038ca4cb5dfa233df91e7708
ljfa-ag <ljfa-ag@web.de>
2015-09-28 14:34:20 +0200

    Change creative tab icon
    Add Buffalo spawn egg to tab

--- src/main/java/totemic_commons/pokefenn/entity/ModEntities.java
+++ src/main/java/totemic_commons/pokefenn/entity/ModEntities.java
@@ -11,9 +11,12 @@ import totemic_commons.pokefenn.lib.Strings;
  */
 public final class ModEntities
 {
+    public static int buffalo;
+
     public static void init()
     {
-        EntityRegistry.registerGlobalEntityID(EntityBuffalo.class, Strings.BUFFALO_NAME, EntityRegistry.findGlobalUniqueEntityId(), 0x2a1c12, 0x885f3e);
+        buffalo = EntityRegistry.findGlobalUniqueEntityId();
+        EntityRegistry.registerGlobalEntityID(EntityBuffalo.class, Strings.BUFFALO_NAME, buffalo, 0x2a1c12, 0x885f3e);
         EntityRegistry.registerModEntity(EntityBuffalo.class, Strings.BUFFALO_NAME, 0, Totemic.instance, 80, 5, true);
         //EntityRegistry.addSpawn(EntityBuffalo.class, 1000, 2, 4, EnumCreatureType.creature); //No biomes to spawn in
     }
--- src/main/java/totemic_commons/pokefenn/misc/CreativeTabTotemic.java
+++ src/main/java/totemic_commons/pokefenn/misc/CreativeTabTotemic.java
@@ -1,22 +1,34 @@
 package totemic_commons.pokefenn.misc;
 
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import totemic_commons.pokefenn.ModItems;
+import totemic_commons.pokefenn.entity.ModEntities;
 
 public class CreativeTabTotemic extends CreativeTabs
 {
-
-    public CreativeTabTotemic(int par1, String par2Str)
+    public CreativeTabTotemic(int id, String name)
     {
-        super(par1, par2Str);
+        super(id, name);
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
     public Item getTabIconItem()
     {
-        return ModItems.totemWhittlingKnife;
+        return ModItems.tipi;
     }
 
-
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void displayAllReleventItems(List list) {
+        super.displayAllReleventItems(list);
+        list.add(new ItemStack(Items.spawn_egg, 1, ModEntities.buffalo));
+    }
 }
