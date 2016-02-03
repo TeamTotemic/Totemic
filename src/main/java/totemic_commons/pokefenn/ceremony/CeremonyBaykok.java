b79b812a4a8d3f970d85e5867c020dc400bd26d5
ljfa-ag <ljfa-ag@web.de>
2016-02-03 16:28:23 +0100

    Add Baykok summoning ceremony

---
+++ src/main/java/totemic_commons/pokefenn/ceremony/CeremonyBaykok.java
@@ -0,0 +1,25 @@
+package totemic_commons.pokefenn.ceremony;
+
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import totemic_commons.pokefenn.api.ceremony.Ceremony;
+import totemic_commons.pokefenn.api.music.MusicInstrument;
+import totemic_commons.pokefenn.entity.boss.EntityBaykok;
+import totemic_commons.pokefenn.util.EntityUtil;
+
+public class CeremonyBaykok extends Ceremony
+{
+    public CeremonyBaykok(String name, int musicNeeded, int maxStartupTime, MusicInstrument... instruments)
+    {
+        super(name, musicNeeded, maxStartupTime, instruments);
+    }
+
+    @Override
+    public void effect(World world, BlockPos pos)
+    {
+        BlockPos spos = pos.offset(EnumFacing.getHorizontal(world.rand.nextInt(4)));
+        EntityUtil.spawnEntity(world, spos.getX() + 0.5, spos.getY(), spos.getZ() + 0.5, new EntityBaykok(world));
+    }
+
+}
