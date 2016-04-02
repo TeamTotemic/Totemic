9de8b9cb5b7603408eb540c503d4494be312b511
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-04-02 22:15:34 +0200

    Add texture for invisible arrows

--- src/main/java/totemic_commons/pokefenn/client/rendering/entity/InvisArrowRendering.java
+++ src/main/java/totemic_commons/pokefenn/client/rendering/entity/InvisArrowRendering.java
@@ -1,5 +1,8 @@
 package totemic_commons.pokefenn.client.rendering.entity;
 
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderArrow;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.projectile.EntityArrow;
@@ -8,7 +11,7 @@ import totemic_commons.pokefenn.entity.projectile.EntityInvisArrow;
 
 public class InvisArrowRendering extends RenderArrow
 {
-    private static final ResourceLocation arrowTextures = new ResourceLocation("textures/entity/arrow.png"); //TODO
+    private static final ResourceLocation arrowTextures = new ResourceLocation("totemic:textures/models/baykokArrow.png");
 
     public InvisArrowRendering(RenderManager renderMgr)
     {
@@ -19,7 +22,13 @@ public class InvisArrowRendering extends RenderArrow
     public void doRender(EntityArrow entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
         if(((EntityInvisArrow)entity).isShotByPlayer())
+        {
+            GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             super.doRender(entity, x, y, z, entityYaw, partialTicks);
+            GL11.glPopAttrib();
+        }
     }
 
     @Override
