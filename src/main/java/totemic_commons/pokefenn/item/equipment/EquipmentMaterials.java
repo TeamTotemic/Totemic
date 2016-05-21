f798738f03510624d513476627cd859a2ec33230
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-05-21 11:37:02 +0200

    Fix crash on adding armor material

--- src/main/java/totemic_commons/pokefenn/item/equipment/EquipmentMaterials.java
+++ src/main/java/totemic_commons/pokefenn/item/equipment/EquipmentMaterials.java
@@ -2,6 +2,7 @@ package totemic_commons.pokefenn.item.equipment;
 
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.common.util.EnumHelper;
 
 /**
@@ -10,15 +11,7 @@ import net.minecraftforge.common.util.EnumHelper;
  */
 public class EquipmentMaterials
 {
-    //public static final ArmorMaterial totemArmour = EnumHelper.addArmorMaterial("totemArmour", "totemArmour", 100, new int[]{1, 4, 3, 1}, 18);
-
-    //public static final ToolMaterial huntingKnife = EnumHelper.addToolMaterial("huntingKnife", 0, 126, 0, -2, 0);
-
-    public static final ArmorMaterial jingleDress = EnumHelper.addArmorMaterial("jingleDress", "jingleDress", 126, new int[]{1, 1, 1, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC);
-
-    //public static final ArmorMaterial warBonnet = EnumHelper.addArmorMaterial("warBonnet", "warBonnet", 256, new int[]{3, 0, 0, 0}, 18);
-
-    //public static final ToolMaterial tomahawk = EnumHelper.addToolMaterial("tomahawk", 0, 256, 0, 5, 0);
-
-    //public static final ArmorMaterial buffalo = EnumHelper.addArmorMaterial("buffalo", "buffalo", 128, new int[]{2, 4, 3, 1}, 21, SoundEvents.item_armor_equip_leather);
+    //EnumHelper is broken at the moment
+    public static final ArmorMaterial jingleDress = EnumHelper.addEnum(ArmorMaterial.class, "jingleDress", new Class<?>[] {String.class, int.class, int[].class, int.class, SoundEvent.class, float.class},
+            "jingleDress", 126, new int[]{1, 1, 1, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0f);
 }
