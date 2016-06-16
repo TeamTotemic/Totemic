1a64837ab8bf152ad7373fd06dc874a501758899
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-06-16 21:16:32 +0200

    Baykok's bow can now shoot non-regular arrows as well
    (but they will not be invisible)
    Also, arrows shot from Baykok's bow no longer slow unless shot by
    Baykok.

--- src/main/java/totemic_commons/pokefenn/entity/projectile/EntityInvisArrow.java
+++ src/main/java/totemic_commons/pokefenn/entity/projectile/EntityInvisArrow.java
@@ -4,13 +4,16 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import totemic_commons.pokefenn.entity.boss.EntityBaykok;
 import totemic_commons.pokefenn.util.EntityUtil;
 
 public class EntityInvisArrow extends EntityArrow
@@ -34,6 +37,13 @@ public class EntityInvisArrow extends EntityArrow
         {
             dataManager.set(SHOOTER_DATAWATCHER, shooter.getEntityId());
         }
+    }
+
+    @Override
+    protected void arrowHit(EntityLivingBase living)
+    {
+        if(shootingEntity instanceof EntityBaykok)
+            living.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 20, 1));
     }
 
     @Override
