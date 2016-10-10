011e60de16509695ac5cd7cc74b1d4e793de75ff
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-10-10 15:58:34 +0200

    Add music amount for StateTotemEffect

--- src/main/java/totemic_commons/pokefenn/tileentity/totem/StateTotemEffect.java
+++ src/main/java/totemic_commons/pokefenn/tileentity/totem/StateTotemEffect.java
@@ -1,7 +1,11 @@
 package totemic_commons.pokefenn.tileentity.totem;
 
+import totemic_commons.pokefenn.api.music.MusicInstrument;
+
 public class StateTotemEffect extends TotemState
 {
+    private int musicAmount = 0;
+
     public StateTotemEffect(TileTotemBase tile)
     {
         super(tile);
@@ -15,5 +19,25 @@ public class StateTotemEffect extends TotemState
             int vertical = e.getElement().getVerticalRange();
             e.getElement().effect(tile.getWorld(), tile.getPos(), tile, e.getCount(), horizontal, vertical);
         });
+
+        //Diminish melody over time
+        if(musicAmount > 0 && tile.getWorld().getTotalWorldTime() % 47 == 0)
+        {
+            musicAmount--;
+            tile.markDirty();
+        }
+    }
+
+    @Override
+    public boolean addMusic(MusicInstrument instr, int amount)
+    {
+        int previous = musicAmount;
+        musicAmount = Math.min(previous + amount / 2, TileTotemBase.MAX_EFFECT_MUSIC);
+        return musicAmount > previous;
+    }
+
+    public int getMusicAmount()
+    {
+        return musicAmount;
     }
 }
