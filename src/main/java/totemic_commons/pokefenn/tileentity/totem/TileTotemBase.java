971de36585c81ed1b068ea9bbcb054dbed322177
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-10-15 14:35:24 +0200

    Add update packet for totem base

--- src/main/java/totemic_commons/pokefenn/tileentity/totem/TileTotemBase.java
+++ src/main/java/totemic_commons/pokefenn/tileentity/totem/TileTotemBase.java
@@ -9,6 +9,9 @@ import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import totemic_commons.pokefenn.Totemic;
@@ -23,7 +26,6 @@ import totemic_commons.pokefenn.tileentity.TileTotemic;
 public class TileTotemBase extends TileTotemic implements MusicAcceptor, TotemBase, ITickable
 {
     public static final int MAX_HEIGHT = 5;
-    public static final int MAX_EFFECT_MUSIC = 128;
 
     private boolean firstTick = true;
 
@@ -168,5 +170,19 @@ public class TileTotemBase extends TileTotemic implements MusicAcceptor, TotemBa
             state = new StateTotemEffect(this);
 
         state.readFromNBT(tag);
+    }
+
+    @Override
+    public Packet getDescriptionPacket()
+    {
+        NBTTagCompound tag = new NBTTagCompound();
+        writeToNBT(tag);
+        return new S35PacketUpdateTileEntity(pos, 0, tag);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
+    {
+        readFromNBT(pkt.getNbtCompound());
     }
 }
