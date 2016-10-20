24ede8b51fd7c17482b975e9bbe12889f10429f4
ljfa-ag <ljfa-ag@users.noreply.github.com>
2016-10-20 21:27:07 +0200

    Create StateCeremonyEffect
    Add reading and writing to NBT for StateStartup

---
+++ src/main/java/totemic_commons/pokefenn/tileentity/totem/StateCeremonyEffect.java
@@ -0,0 +1,82 @@
+package totemic_commons.pokefenn.tileentity.totem;
+
+import static totemic_commons.pokefenn.Totemic.logger;
+
+import net.minecraft.nbt.NBTTagCompound;
+import totemic_commons.pokefenn.Totemic;
+import totemic_commons.pokefenn.api.ceremony.Ceremony;
+import totemic_commons.pokefenn.api.music.MusicInstrument;
+
+public class StateCeremonyEffect extends TotemState
+{
+    public static final int ID = 3;
+
+    private Ceremony ceremony;
+    private int time = 0;
+
+    public StateCeremonyEffect(TileTotemBase tile)
+    {
+        super(tile);
+    }
+
+    public StateCeremonyEffect(TileTotemBase tile, Ceremony ceremony)
+    {
+        this(tile);
+        this.ceremony = ceremony;
+    }
+
+    @Override
+    public void update()
+    {
+        ceremony.effect(tile.getWorld(), tile.getPos(), time);
+
+        if(!tile.getWorld().isRemote)
+        {
+            if(time >= ceremony.getEffectTime())
+                tile.setState(new StateTotemEffect(tile));
+        }
+        else
+        {
+            //Due to network delay, we want to avoid ticking instant ceremonies more than once on the client side
+            if(ceremony.getEffectTime() == Ceremony.INSTANT)
+                tile.setState(new StateTotemEffect(tile));
+
+            tile.setCeremonyOverlay();
+        }
+
+        time++;
+    }
+
+    @Override
+    public boolean addMusic(MusicInstrument instr, int amount)
+    {
+        return false;
+    }
+
+    @Override
+    public int getID()
+    {
+        return ID;
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag)
+    {
+        tag.setString("ceremony", ceremony.getName());
+        tag.setInteger("time", time);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag)
+    {
+        ceremony = Totemic.api.registry().getCeremony(tag.getString("ceremony"));
+        if(ceremony == null)
+        {
+            logger.warn("Unknown ceremony: {}", tag.getString("ceremony"));
+            tile.setState(new StateTotemEffect(tile));
+        }
+
+        time = tag.getInteger("time");
+    }
+
+}
