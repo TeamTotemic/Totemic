d601ab1af73acc9bff706f732726eb4a8fbae44b
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-02-22 19:40:32 +0100

    Add compatibility with Animania cows. Closes #45

--- src/main/java/totemic_commons/pokefenn/ceremony/CeremonyBuffaloDance.java
+++ src/main/java/totemic_commons/pokefenn/ceremony/CeremonyBuffaloDance.java
@@ -1,8 +1,12 @@
 package totemic_commons.pokefenn.ceremony;
 
+import java.util.List;
+
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Loader;
 import totemic_commons.pokefenn.api.ceremony.Ceremony;
 import totemic_commons.pokefenn.api.music.MusicInstrument;
 import totemic_commons.pokefenn.entity.animal.EntityBuffalo;
@@ -21,8 +25,7 @@ public class CeremonyBuffaloDance extends Ceremony
         if(world.isRemote)
             return;
 
-        EntityUtil.getEntitiesInRange(EntityCow.class, world, pos, 8, 8).stream()
-            .filter(entity -> !(entity instanceof EntityBuffalo))
+        getCows(world, pos, 8).stream()
             .limit(2)
             .forEach(cow -> {
                 EntityBuffalo buffalo = new EntityBuffalo(world);
@@ -31,5 +34,16 @@ public class CeremonyBuffaloDance extends Ceremony
                 EntityUtil.spawnEntity(world, cow.posX, cow.posY, cow.posZ, buffalo);
                 cow.setDead();
             });
+    }
+
+    private static List<? extends EntityAnimal> getCows(World world, BlockPos pos, int range)
+    {
+        if(!Loader.isModLoaded("animania"))
+            return EntityUtil.getEntitiesInRange(EntityCow.class, world, pos, range, range, entity -> !(entity instanceof EntityBuffalo));
+        else //Animania compatibility
+            return EntityUtil.getEntitiesInRange(EntityAnimal.class, world, pos, range, range, entity ->
+                  (entity instanceof EntityCow && !(entity instanceof EntityBuffalo))
+                || entity.getClass().getName().startsWith("com.animania.entities.cows.EntityBull")
+                || entity.getClass().getName().startsWith("com.animania.entities.cows.EntityCow"));
     }
 }
