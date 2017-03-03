acfbd55d9672f10053696af8fb6aedf6a9b2c29e
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-03-03 15:41:55 +0100

    Remove deprecated methods from TotemEffectAPI

--- src/main/java/totemic_commons/pokefenn/api/totem/TotemEffectAPI.java
+++ src/main/java/totemic_commons/pokefenn/api/totem/TotemEffectAPI.java
@@ -1,17 +1,9 @@
 package totemic_commons.pokefenn.api.totem;
 
-import java.util.Random;
-
-import com.google.common.annotations.Beta;
-
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-
 /**
  * Provides access to functionality commonly used for Totem Effects.
  * Use {@code TotemicAPI.get().totemEffect()} to get an instance of this interface.
  */
-@Beta
 public interface TotemEffectAPI
 {
     /**
@@ -37,42 +29,4 @@ public interface TotemEffectAPI
      * @see #getDefaultRange(TotemEffect, TotemBase, int)
      */
     int getDefaultRange(TotemEffect effect, int baseRange, TotemBase totem, int repetition);
-
-    /**
-     * Calculates how long a potion effect from a Totem Pole should last by default.
-     * The value is calculated based on the properties of the Totem Base.
-     * @param baseTime the base time
-     * @param isBad whether the effect is bad for the player
-     * @param totem the Totem Base tile entity where the effect originates
-     * @param repetition the number of Totem Pole blocks in the pole that are carved with this effect
-     * @return the default time how long a potion effect from a Totem effect lasts
-     * @deprecated Replaced by suitable methods in {@link TotemEffectPotion}. Will be removed.
-     */
-    @Deprecated
-    int getDefaultPotionTime(int baseTime, boolean isBad, Random rand, TotemBase totem, int repetition);
-
-    /**
-     * Calculates how strong a potion effect from a Totem Pole should be by default.
-     * The value is calculated based on the properties of the Totem Base.
-     * @param baseStrength the base strength
-     * @param isBad whether the effect is bad for the player
-     * @param totem the Totem Base tile entity where the effect originates
-     * @param repetition the number of Totem Pole blocks in the pole that are carved with this effect
-     * @return the default strength of a potion effect from a Totem effect
-     * @deprecated Replaced by suitable methods in {@link TotemEffectPotion}. Will be removed.
-     */
-    @Deprecated
-    int getDefaultPotionStrength(int baseStrength, boolean isBad, Random rand, TotemBase totem, int repetition);
-
-    /**
-     * Adds a potion effect to the entity, whose time and strength
-     * are calculated from getDefaultPotionTime() and getDefaultPotionStrength()
-     * @param baseTime the base time
-     * @param baseStrength the base strength
-     * @param totem the Totem Base tile entity where the effect originates
-     * @param repetition the number of Totem Pole blocks in the pole that are carved with this effect
-     * @deprecated Replaced by suitable methods in {@link TotemEffectPotion}. Will be removed.
-     */
-    @Deprecated
-    void addPotionEffect(EntityLivingBase entity, Potion potion, int baseTime, int baseStrength, TotemBase totem, int repetition);
 }
--- src/main/java/totemic_commons/pokefenn/apiimpl/totem/TotemEffectApiImpl.java
+++ src/main/java/totemic_commons/pokefenn/apiimpl/totem/TotemEffectApiImpl.java
@@ -1,10 +1,5 @@
 package totemic_commons.pokefenn.apiimpl.totem;
 
-import java.util.Random;
-
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import totemic_commons.pokefenn.api.totem.TotemBase;
 import totemic_commons.pokefenn.api.totem.TotemEffect;
 import totemic_commons.pokefenn.api.totem.TotemEffectAPI;
@@ -21,26 +16,5 @@ public class TotemEffectApiImpl implements TotemEffectAPI
     public int getDefaultRange(TotemEffect effect, int baseRange, TotemBase totem, int repetition)
     {
         return baseRange + totem.getTotemEffectMusic() / 32 + (totem.getPoleSize() >= TotemBase.MAX_POLE_SIZE ? 1 : 0);
-    }
-
-    @Override
-    public int getDefaultPotionTime(int baseTime, boolean isBad, Random rand, TotemBase totem, int repetition)
-    {
-        return baseTime + 20;
-    }
-
-    @Override
-    public int getDefaultPotionStrength(int baseStrength, boolean isBad, Random rand, TotemBase totem, int repetition)
-    {
-        return baseStrength + (repetition - 1) / 2 + (totem.getTotemEffectMusic() > 96 ? 1 : 0);
-    }
-
-    @Override
-    public void addPotionEffect(EntityLivingBase entity, Potion potion, int baseTime, int baseStrength, TotemBase totem, int repetition)
-    {
-        entity.addPotionEffect(new PotionEffect(potion,
-                getDefaultPotionTime(baseTime, potion.isBadEffect(), entity.getRNG(), totem, repetition),
-                getDefaultPotionStrength(baseStrength, potion.isBadEffect(), entity.getRNG(), totem, repetition),
-                true, false));
     }
 }
