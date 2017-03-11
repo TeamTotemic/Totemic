475b7042dcde35efc4ff52bee05ce3b42aa4da5b
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-03-11 19:23:56 +0100

    Fix entity registration not being backwards compatible

--- src/main/java/totemic_commons/pokefenn/entity/ModEntities.java
+++ src/main/java/totemic_commons/pokefenn/entity/ModEntities.java
@@ -12,8 +12,8 @@ public final class ModEntities
 {
     public static void init()
     {
-        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.BUFFALO_NAME), EntityBuffalo.class, Strings.BUFFALO_NAME, 0, Totemic.instance, 80, 5, true, 0x2a1c12, 0x885f3e);
-        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.BAYKOK_NAME), EntityBaykok.class, Strings.BAYKOK_NAME, 1, Totemic.instance, 80, 3, true, 0xE0E0E0, 0xF8DAD2);
-        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.INVIS_ARROW_NAME), EntityInvisArrow.class, Strings.INVIS_ARROW_NAME, 2, Totemic.instance, 64, 20, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID + "." + Strings.BUFFALO_NAME), EntityBuffalo.class, Strings.BUFFALO_NAME, 0, Totemic.instance, 80, 5, true, 0x2a1c12, 0x885f3e);
+        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID + "." + Strings.BAYKOK_NAME), EntityBaykok.class, Strings.BAYKOK_NAME, 1, Totemic.instance, 80, 3, true, 0xE0E0E0, 0xF8DAD2);
+        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID + "." + Strings.INVIS_ARROW_NAME), EntityInvisArrow.class, Strings.INVIS_ARROW_NAME, 2, Totemic.instance, 64, 20, true);
     }
 }
