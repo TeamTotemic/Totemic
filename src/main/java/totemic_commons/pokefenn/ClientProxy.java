8894917f45c7eb20484db240695958447ae1e5ff
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-03-19 13:20:21 +0100

    Add something for April Fools
    Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo

--- src/main/java/totemic_commons/pokefenn/ClientProxy.java
+++ src/main/java/totemic_commons/pokefenn/ClientProxy.java
@@ -2,12 +2,18 @@ package totemic_commons.pokefenn;
 
 import static totemic_commons.pokefenn.Totemic.logger;
 
+import java.util.Calendar;
+import java.util.Map;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.Level;
 import org.lwjgl.opengl.Display;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.*;
+import net.minecraft.util.text.translation.LanguageMap;
 import net.minecraft.world.ColorizerFoliage;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.common.MinecraftForge;
@@ -17,6 +23,7 @@ import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import totemic_commons.pokefenn.client.rendering.entity.BaykokRendering;
 import totemic_commons.pokefenn.client.rendering.entity.BuffaloRendering;
 import totemic_commons.pokefenn.client.rendering.entity.InvisArrowRendering;
@@ -67,6 +74,13 @@ public class ClientProxy extends CommonProxy
         {
             logger.catching(Level.WARN, e);
         }
+
+        Calendar calendar = Calendar.getInstance();
+        if(calendar.get(Calendar.DAY_OF_MONTH) == 1 && calendar.get(Calendar.MONTH) == Calendar.APRIL)
+        {
+            ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener(new AprilFoolsReloadListener());
+            logger.info("Happy April Fools!");
+        }
     }
 
     @Override
@@ -86,5 +100,60 @@ public class ClientProxy extends CommonProxy
     private void initTESRs()
     {
         ClientRegistry.bindTileEntitySpecialRenderer(TileWindChime.class, new TileWindChimeRenderer());
+    }
+
+    private static class AprilFoolsReloadListener implements IResourceManagerReloadListener
+    {
+        @Override
+        public void onResourceManagerReload(IResourceManager resourceManager)
+        {
+            try
+            {
+                //Hacky solution to get the current translation map
+                Locale locale = ReflectionHelper.getPrivateValue(LanguageManager.class, null, 3);
+                Map<String, String> lang = ReflectionHelper.getPrivateValue(Locale.class, locale, 2);
+
+                for(Map.Entry<String, String> entry: lang.entrySet())
+                {
+                    String key = entry.getKey();
+                    if(key.startsWith("totemic") || key.startsWith("item.totemic:") || key.startsWith("tile.totemic:"))
+                    {
+                        entry.setValue(buffaloify(entry.getValue()));
+                    }
+                }
+
+                LanguageMap.replaceWith(lang);
+            }
+            catch(Exception e)
+            {
+                logger.catching(Level.WARN, e);
+            }
+        }
+
+        private static final Pattern word = Pattern.compile("[A-Za-z][a-z][a-z][a-z]+");
+
+        //Replace every word with at least three letters by "buffalo" or "Buffalo"
+        private static String buffaloify(String text)
+        {
+            try
+            {
+                Matcher matcher = word.matcher(text);
+                StringBuffer sb = new StringBuffer();
+
+                while(matcher.find())
+                {
+                    if(Character.isUpperCase(text.charAt(matcher.start())))
+                        matcher.appendReplacement(sb, "Buffalo");
+                    else
+                        matcher.appendReplacement(sb, "buffalo");
+                }
+                matcher.appendTail(sb);
+                return sb.toString();
+            }
+            catch(Exception e)
+            {
+                return text;
+            }
+        }
     }
 }
