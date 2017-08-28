dceabe4cac3488a066bcf262d1763439a18c4d2a
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-08-28 11:10:30 +0200

    Reenable velocity updates for invisible arrows

--- src/main/java/pokefenn/totemic/CommonProxy.java
+++ src/main/java/pokefenn/totemic/CommonProxy.java
@@ -68,7 +68,7 @@ public class CommonProxy
     {
         EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.BUFFALO_NAME), EntityBuffalo.class, Strings.RESOURCE_PREFIX + Strings.BUFFALO_NAME, 0, Totemic.instance, 80, 3, true, 0x2A1C12, 0x885F3E);
         EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.BAYKOK_NAME), EntityBaykok.class, Strings.RESOURCE_PREFIX + Strings.BAYKOK_NAME, 1, Totemic.instance, 80, 3, true, 0xE0E0E0, 0xF8DAD2);
-        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.INVIS_ARROW_NAME), EntityInvisArrow.class, Strings.RESOURCE_PREFIX + Strings.INVIS_ARROW_NAME, 2, Totemic.instance, 64, 20, false);
+        EntityRegistry.registerModEntity(new ResourceLocation(Totemic.MOD_ID, Strings.INVIS_ARROW_NAME), EntityInvisArrow.class, Strings.RESOURCE_PREFIX + Strings.INVIS_ARROW_NAME, 2, Totemic.instance, 64, 20, true);
     }
 
     private void registerTileEntities()
