f05a669937414e1334113e46e56eb3512b0e4f9c
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-10-06 15:22:01 +0200

    Add Jar signing (thanks Darkhax)

--- src/main/java/pokefenn/totemic/Totemic.java
+++ src/main/java/pokefenn/totemic/Totemic.java
@@ -9,8 +9,6 @@ import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -21,7 +19,7 @@ import pokefenn.totemic.api.TotemicAPI;
 import pokefenn.totemic.apiimpl.ApiImpl;
 
 @Mod(modid = Totemic.MOD_ID, name = Totemic.MOD_NAME, version = Totemic.MOD_VERSION, acceptedMinecraftVersions = "[1.12.2,)",
-        dependencies = "required-after:forge@[14.23.0.2491,)",
+        certificateFingerprint = "@FINGERPRINT@", dependencies = "required-after:forge@[14.23.0.2491,)",
         updateJSON = "https://raw.githubusercontent.com/TeamTotemic/Totemic/version/version.json")
 public final class Totemic
 {
@@ -29,7 +27,7 @@ public final class Totemic
     public static final String MOD_NAME = "Totemic";
     public static final String MOD_VERSION = "${version}";
 
-    @Instance
+    @Mod.Instance
     public static Totemic instance;
 
     @SidedProxy(clientSide = "pokefenn.totemic.ClientProxy", serverSide = "pokefenn.totemic.CommonProxy")
@@ -39,7 +37,7 @@ public final class Totemic
     public static final CreativeTabs tabsTotem = new CreativeTabTotemic(MOD_ID);
     public static final Logger logger = LogManager.getLogger(MOD_NAME);
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger.info("Moma had a cow, Moma had a chicken... Dad was proud, he didn't care how!");
@@ -49,14 +47,14 @@ public final class Totemic
         proxy.preInit(event);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         logger.info("Totemic is entering initialization stage");
         proxy.init(event);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
         logger.info("Totemic is entering postinitialization stage");
