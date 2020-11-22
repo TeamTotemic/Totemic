f71c89bb475f134b90c0dcb5bdfeaef4f88407e5
ljfa <ljfa-ag@users.noreply.github.com>
2020-11-22 18:55:27 +0100

    Fix crash with wrong event type
    Update generated resources

--- src/main/java/pokefenn/totemic/api/totem/RegisterTotemEffectsEvent.java
+++ src/main/java/pokefenn/totemic/api/totem/RegisterTotemEffectsEvent.java
@@ -4,13 +4,14 @@ import java.util.function.Consumer;
 
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.lifecycle.IModBusEvent;
 
 /**
  * Register your Totem Effects when you receive this event, using the {@link #register} or {@link #registerAll} methods.
  * Please do <b>not</b> use Forge's {@link RegistryEvent}!<p>
  * This allows us to register Totem Effects before blocks, so we can dynamically add Totem Pole blocks for each Totem Effect.
  */
-public class RegisterTotemEffectsEvent extends Event {
+public class RegisterTotemEffectsEvent extends Event implements IModBusEvent {
     private final Consumer<TotemEffect> registry;
 
     public RegisterTotemEffectsEvent(Consumer<TotemEffect> registry) {
