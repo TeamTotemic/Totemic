02cbdc0c85393d4203c47992b00cb9b665e97050
ljfa <ljfa-ag@users.noreply.github.com>
2022-05-26 21:28:05 +0200

    Band aid fix for incorrectly rotated Buffalo Totem Pole model

--- src/main/java/pokefenn/totemic/data/TotemicBlockStateProvider.java
+++ src/main/java/pokefenn/totemic/data/TotemicBlockStateProvider.java
@@ -14,6 +14,7 @@ import pokefenn.totemic.Totemic;
 import pokefenn.totemic.block.totem.TotemBaseBlock;
 import pokefenn.totemic.block.totem.TotemPoleBlock;
 import pokefenn.totemic.init.ModBlocks;
+import pokefenn.totemic.init.ModContent;
 
 public class TotemicBlockStateProvider extends BlockStateProvider {
     public TotemicBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -42,19 +43,26 @@ public class TotemicBlockStateProvider extends BlockStateProvider {
                     .texture("particle", block.woodType.getParticleTexture());
 
             //Block state
-            waterloggedHorizontalBlock(block, blockModel);
+            if(block.effect == ModContent.buffalo) //FIXME: Buffalo Totem Pole model is still rotated incorrectly
+                waterloggedHorizontalBlock(block, blockModel, 270);
+            else
+                waterloggedHorizontalBlock(block, blockModel);
             //Item model
             itemModels().getBuilder(block.getRegistryName().toString()).parent(blockModel);
         }
     }
 
     // The same as BlockStateProvider#horizontalBlock, but ignoring the Waterlogged property
-    private void waterloggedHorizontalBlock(Block block, ModelFile model) {
+    private void waterloggedHorizontalBlock(Block block, ModelFile model, int angleOffset) {
         getVariantBuilder(block)
             .forAllStatesExcept(state -> ConfiguredModel.builder()
                     .modelFile(model)
-                    .rotationY(((int) state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot() + 180) % 360)
+                    .rotationY(((int) state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot() + angleOffset) % 360)
                     .build(),
             BlockStateProperties.WATERLOGGED);
+    }
+
+    private void waterloggedHorizontalBlock(Block block, ModelFile model) {
+        waterloggedHorizontalBlock(block, model, 180);
     }
 }
