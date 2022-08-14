6dce78726670f816ddaf2fa97fd4070e7bc5ba3c
ljfa <ljfa-ag@users.noreply.github.com>
2022-08-14 15:29:37 +0200

    Fix another ArrayStoreException
    Now launching without errors in MC 1.19

--- src/main/java/pokefenn/totemic/handler/ModBlockColors.java
+++ src/main/java/pokefenn/totemic/handler/ModBlockColors.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.RegistryObject;
 import pokefenn.totemic.block.totem.TotemPoleBlock;
 import pokefenn.totemic.init.ModBlocks;
 
@@ -11,12 +12,16 @@ public class ModBlockColors {
     @SubscribeEvent
     public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
         event.register((state, tintGetter, pos, tintIndex) -> TotemPoleBlock.getBlockColor(tintIndex),
-                ModBlocks.getTotemPoles().values().toArray(Block[]::new));
+                ModBlocks.getTotemPoles().values().stream()
+                        .map(RegistryObject::get)
+                        .toArray(Block[]::new));
     }
 
     @SubscribeEvent
     public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
         event.register((stack, tintIndex) -> TotemPoleBlock.getBlockColor(tintIndex),
-                ModBlocks.getTotemPoles().values().toArray(ItemLike[]::new));
+                ModBlocks.getTotemPoles().values().stream()
+                        .map(RegistryObject::get)
+                        .toArray(ItemLike[]::new));
     }
 }
