73b0852ff03f95c8d0cbc39c945d94bd99d597d8
ljfa <ljfa-ag@users.noreply.github.com>
2022-11-12 13:00:48 +0100

    Only get the unbaked model once per totem effect
    Copying the unbaked model seems to be unnecessary

--- src/main/java/pokefenn/totemic/handler/ClientInitHandlers.java
+++ src/main/java/pokefenn/totemic/handler/ClientInitHandlers.java
@@ -1,5 +1,6 @@
 package pokefenn.totemic.handler;
 
+import com.google.common.base.Stopwatch;
 import com.mojang.datafixers.util.Either;
 
 import net.minecraft.client.Minecraft;
@@ -17,6 +18,7 @@ import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.RegistryObject;
+import pokefenn.totemic.Totemic;
 import pokefenn.totemic.api.TotemicAPI;
 import pokefenn.totemic.api.totem.TotemEffect;
 import pokefenn.totemic.block.totem.TotemPoleBlock;
@@ -58,29 +60,35 @@ public class ClientInitHandlers {
     @SubscribeEvent
     //FIXME: This does work but it spams the log since the block state definitions and item models can't be loaded
     public static void onBakingComplete(ModelEvent.BakingCompleted event) {
-        for(var blockO: ModBlocks.getTotemPoles().values()) {
-            var blockName = blockO.getId();
-            var block = blockO.get();
-            var woodType = block.woodType;
-            var modelName = getPoleModelName(block.effect);
+        var stopwatch = Stopwatch.createStarted();
+        for(var totemEffectEntry: ModBlocks.getTotemPoles().columnMap().entrySet()) {
+            var modelName = getPoleModelName(totemEffectEntry.getKey());
             var unbakedModel = (BlockModel) event.getModelBakery().getModel(modelName);
 
-            var retexturedModel = copyBlockModel(unbakedModel);
-            retexturedModel.textureMap.put("wood", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getWoodTexture()))));
-            retexturedModel.textureMap.put("bark", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getBarkTexture()))));
-            retexturedModel.textureMap.put("top", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getTopTexture()))));
-            retexturedModel.textureMap.put("particle", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getParticleTexture()))));
+            for(var blockO: totemEffectEntry.getValue().values()) {
+                var blockName = blockO.getId();
+                var block = blockO.get();
+                var woodType = block.woodType;
 
-            //Block models
-            for(var state: block.getStateDefinition().getPossibleStates()) {
-                var bakedModel = retexturedModel.bake(event.getModelBakery(), retexturedModel, event.getModelBakery().getAtlasSet()::getSprite,
-                        BlockModelRotation.by(0, (int) state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot() + 180), modelName, false);
-                event.getModels().put(BlockModelShaper.stateToModelLocation(blockName, state), bakedModel);
+                //The unbaked model will be modified here, but that should be fine since it is not used elsewhere
+                unbakedModel.textureMap.replace("wood", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getWoodTexture()))));
+                unbakedModel.textureMap.replace("bark", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getBarkTexture()))));
+                unbakedModel.textureMap.replace("top", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getTopTexture()))));
+                unbakedModel.textureMap.replace("particle", Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(woodType.getParticleTexture()))));
+
+                //Block models
+                for(var state: block.getStateDefinition().getPossibleStates()) {
+                    var bakedModel = unbakedModel.bake(event.getModelBakery(), unbakedModel, event.getModelBakery().getAtlasSet()::getSprite,
+                            BlockModelRotation.by(0, (int) state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot() + 180), modelName, false);
+                    event.getModels().put(BlockModelShaper.stateToModelLocation(blockName, state), bakedModel);
+                }
+                //Item model
+                event.getModels().put(new ModelResourceLocation(blockName, "inventory"),
+                        unbakedModel.bake(event.getModelBakery(), unbakedModel, event.getModelBakery().getAtlasSet()::getSprite, BlockModelRotation.X0_Y0, modelName, true));
             }
-            //Item model
-            event.getModels().put(new ModelResourceLocation(blockName, "inventory"),
-                    retexturedModel.bake(event.getModelBakery(), retexturedModel, event.getModelBakery().getAtlasSet()::getSprite, BlockModelRotation.X0_Y0, modelName, true));
         }
+        stopwatch.stop();
+        Totemic.logger.info("Totem Pole model baking took {}", stopwatch);
 
         if(!Minecraft.useFancyGraphics()) {
             //Replace all the occurrences of the cedar leaves model with opaque ones.
@@ -96,10 +104,5 @@ public class ClientInitHandlers {
     private static ResourceLocation getPoleModelName(TotemEffect effect) {
         var effectName = effect.getRegistryName();
         return new ResourceLocation(effectName.getNamespace(), "block/totem_pole_" + effectName.getPath());
-    }
-
-    @SuppressWarnings("deprecation")
-    private static BlockModel copyBlockModel(BlockModel model) {
-        return new BlockModel(model.getParentLocation(), model.getElements(), model.textureMap, model.hasAmbientOcclusion, model.getGuiLight(), model.getTransforms(), model.getOverrides());
     }
 }
