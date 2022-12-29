225f0bb0c2fb709928147ba99428d0d422bf9fe9
ljfa <ljfa-ag@users.noreply.github.com>
2022-12-29 17:22:27 +0100

    Add invisible arrow to the Arrows entity type tag

--- src/main/java/pokefenn/totemic/data/TotemicEntityTypeTagsProvider.java
+++ src/main/java/pokefenn/totemic/data/TotemicEntityTypeTagsProvider.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import pokefenn.totemic.init.ModEntityTypes;
@@ -15,6 +16,7 @@ public class TotemicEntityTypeTagsProvider extends EntityTypeTagsProvider {
 
     @Override
     protected void addTags() {
+        tag(EntityTypeTags.ARROWS).add(ModEntityTypes.invisible_arrow.get());
         tag(Tags.EntityTypes.BOSSES).add(ModEntityTypes.baykok.get());
     }
 
