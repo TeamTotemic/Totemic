8fcbb1fac3495c0e5f02e547a68986e5b422b1cc
ljfa <ljfa-ag@users.noreply.github.com>
2023-02-13 11:14:25 +0100

    Remove TotemicStaffUsable interface

--- src/main/java/pokefenn/totemic/item/TotemicStaffItem.java
+++ src/main/java/pokefenn/totemic/item/TotemicStaffItem.java
@@ -6,19 +6,14 @@ import javax.annotation.Nullable;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import pokefenn.totemic.api.TotemicStaffUsable;
 import pokefenn.totemic.block.totem.TotemBaseBlock;
 
-@SuppressWarnings("deprecation")
 public class TotemicStaffItem extends Item {
     public TotemicStaffItem(Properties props) {
         super(props);
@@ -27,15 +22,6 @@ public class TotemicStaffItem extends Item {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
         tooltip.add(Component.translatable(getDescriptionId() + ".tooltip"));
-    }
-
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Block block = context.getLevel().getBlockState(context.getClickedPos()).getBlock();
-        if(block instanceof TotemicStaffUsable tu)
-            return tu.onTotemicStaffRightClick(context);
-        else
-            return InteractionResult.FAIL;
     }
 
     @Override
