d4f8047083b4efe6f8056583354ae930f14d19e9
ljfa <ljfa-ag@users.noreply.github.com>
2023-02-22 22:15:27 +0100

    Make animal growth ceremony hatch turtle eggs faster as well

--- src/main/java/pokefenn/totemic/ceremony/AnimalGrowthCeremony.java
+++ src/main/java/pokefenn/totemic/ceremony/AnimalGrowthCeremony.java
@@ -2,11 +2,18 @@ package pokefenn.totemic.ceremony;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.TurtleEggBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import pokefenn.totemic.api.TotemicEntityUtil;
 import pokefenn.totemic.api.ceremony.CeremonyEffectContext;
@@ -23,6 +30,7 @@ public enum AnimalGrowthCeremony implements CeremonyInstance {
         if(!level.isClientSide && context.getTime() % 20 == 0) {
             var aabb = TotemicEntityUtil.getAABBAround(pos, RADIUS);
 
+            //Animal growth
             level.getEntitiesOfClass(Animal.class, aabb, Animal::isBaby)
             .forEach(animal -> {
                 if(level.random.nextInt(4) == 0) {
@@ -33,6 +41,7 @@ public enum AnimalGrowthCeremony implements CeremonyInstance {
                 }
             });
 
+            //Chicken egg hatching
             level.getEntities(EntityType.ITEM, aabb, e -> e.getItem().is(Items.EGG))
             .forEach(egg -> {
                 if(level.random.nextInt(4) == 0) {
@@ -44,6 +53,39 @@ public enum AnimalGrowthCeremony implements CeremonyInstance {
                     MiscUtil.shrinkItemEntity(egg);
                 }
             });
+
+            //Turtle egg hatching
+            BlockPos.betweenClosedStream(aabb)
+            .forEach(p -> {
+                var state = level.getBlockState(p);
+                if(state.is(Blocks.TURTLE_EGG) && TurtleEggBlock.onSand(level, p)) {
+                    if(level.random.nextInt(4) == 0)
+                        MiscUtil.spawnServerParticles(ParticleTypes.HAPPY_VILLAGER, level, Vec3.atBottomCenterOf(p), 10, new Vec3(0.5, 0.5, 0.5), 1.0);
+                    if(level.random.nextInt(getEffectTime()) == 0) //about once per ceremony usage
+                        hatchTurtleEgg(state, level, p);
+                }
+            });
+        }
+    }
+
+    private void hatchTurtleEgg(BlockState state, Level level, BlockPos pos) {
+        int age = state.getValue(TurtleEggBlock.HATCH);
+        if(age < 2) {
+            level.playSound(null, pos, SoundEvents.TURTLE_EGG_CRACK, SoundSource.BLOCKS, 0.7F, 0.9F + level.random.nextFloat() * 0.2F);
+            level.setBlock(pos, state.setValue(TurtleEggBlock.HATCH, age + 1), 2);
+        }
+        else {
+            level.playSound(null, pos, SoundEvents.TURTLE_EGG_HATCH, SoundSource.BLOCKS, 0.7F, 0.9F + level.random.nextFloat() * 0.2F);
+            level.removeBlock(pos, false);
+
+            for(int j = 0; j < state.getValue(TurtleEggBlock.EGGS); ++j) {
+                level.levelEvent(2001, pos, Block.getId(state));
+                Turtle turtle = EntityType.TURTLE.create(level);
+                turtle.setAge(AgeableMob.BABY_START_AGE);
+                turtle.setHomePos(pos);
+                turtle.moveTo(pos.getX() + 0.3D + j * 0.2D, pos.getY(), pos.getZ() + 0.3D, 0.0F, 0.0F);
+                level.addFreshEntity(turtle);
+            }
         }
     }
 
