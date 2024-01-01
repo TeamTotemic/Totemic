1db38688ddc523b90de70ad847cae37edaa5cd77
ljfa <ljfa-ag@users.noreply.github.com>
2024-01-01 11:32:22 +0100

    Update NeoForge to 20.4.70 (Networking Refactor)
    Change the way the API is versioned in package-info.java
    
    Update Gradle wrapper to 8.5

--- src/main/java/pokefenn/totemic/api/package-info.java
+++ src/main/java/pokefenn/totemic/api/package-info.java
@@ -5,6 +5,6 @@
  * <p>
  * Feel free to create an issue or pull request on Github to suggest changes or extensions to the API.
  *
- * @version 1.20.2-0.1.0
+ * @version 2.0.0
  */
 package pokefenn.totemic.api;
