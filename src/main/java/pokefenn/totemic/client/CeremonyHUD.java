5959e2129602a6ace4e1e2833f8956b84c204ea8
ljfa <ljfa-ag@users.noreply.github.com>
2024-07-12 00:22:50 +0200

    Fix the last remaining compiler errors in CeremonyHUD

--- src/main/java/pokefenn/totemic/client/CeremonyHUD.java
+++ src/main/java/pokefenn/totemic/client/CeremonyHUD.java
@@ -16,7 +16,6 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.neoforged.neoforge.client.gui.overlay.ExtendedGui;
 import pokefenn.totemic.Totemic;
 import pokefenn.totemic.TotemicConfig;
 import pokefenn.totemic.block.totem.entity.StateCeremonyEffect;
@@ -56,6 +55,7 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
 
         if(activeTotem.isRemoved() || activeTotem.getLevel() != mc.level || activeTotem.getTotemState() instanceof StateTotemEffect) {
             activeTotem = null;
+            mc.getProfiler().pop();
             return;
         }
 
@@ -68,26 +68,27 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
 
         var state = activeTotem.getTotemState();
         if(state instanceof StateSelection s)
-            renderSelectionHUD(s, gui, guiGraphics, partialTick, hudX, hudY);
+            renderSelectionHUD(s, guiGraphics, deltaTracker);
         else if(state instanceof StateStartup s)
-            renderStartupHUD(s, gui, guiGraphics, partialTick);
+            renderStartupHUD(s, guiGraphics, deltaTracker);
         else if(state instanceof StateCeremonyEffect s)
-            renderCeremonyEffectHUD(s, gui, guiGraphics, partialTick);
+            renderCeremonyEffectHUD(s, guiGraphics, deltaTracker);
 
         poseStack.popPose();
 
         mc.getProfiler().pop();
     }
 
-    private void renderSelectionHUD(StateSelection state, ExtendedGui gui, GuiGraphics guiGraphics, float partialTick, int hudX, int hudY) {
+    @SuppressWarnings("resource")
+    private void renderSelectionHUD(StateSelection state, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         final int texW = 128, texH = 64;
-        gui.setupOverlayRenderState(true, false);
 
         //Background
         guiGraphics.blit(SELECTION_HUD_TEXTURE, 0, 0,  0, 0,  HUD_WIDTH, HUD_HEIGHT,  texW, texH);
 
-        int headerX = (HUD_WIDTH - gui.getFont().width(SELECTION_TEXT)) / 2;
-        guiGraphics.drawString(gui.getFont(), SELECTION_TEXT, headerX, 2, 0xC8000000, false);
+        var font = Minecraft.getInstance().font;
+        int headerX = (HUD_WIDTH - font.width(SELECTION_TEXT)) / 2;
+        guiGraphics.drawString(font, SELECTION_TEXT, headerX, 2, 0xC8000000, false);
 
         //Instruments
         var selectors = state.getSelectors();
@@ -99,16 +100,14 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
     }
 
     @SuppressWarnings("resource")
-    private void renderStartupHUD(StateStartup state, ExtendedGui gui, GuiGraphics guiGraphics, float partialTick) {
+    private void renderStartupHUD(StateStartup state, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         final int texW = 128, texH = 64;
         final int barW = 104, barH = 7;
         var cer = state.getCeremony();
 
-        gui.setupOverlayRenderState(true, false);
         RenderSystem.setShaderTexture(0, CEREMONY_HUD_TEXTURE);
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        var buf = Tesselator.getInstance().getBuilder();
-        buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        var buf = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
         var poseStack = guiGraphics.pose();
 
         //Background
@@ -120,29 +119,30 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
 
         //Bars
         float musicW = state.getTotalMusic() / (float)cer.getMusicNeeded() * barW;
-        float timeW = Math.min((state.getTime() + partialTick) / cer.getAdjustedMaxStartupTime(gui.getMinecraft().level.getDifficulty()), 1.0F) * barW;
+        float partialTick = deltaTracker.getGameTimeDeltaPartialTick(true);
+        float timeW = Math.min((state.getTime() + partialTick) / cer.getAdjustedMaxStartupTime(Minecraft.getInstance().level.getDifficulty()), 1.0F) * barW;
         addQuad(buf, poseStack, 11, 11,  musicW, barH,  0, 32,  musicW, barH, texW, texH);
         addQuad(buf, poseStack, 11, 21,  timeW,  barH,  0, 32,  timeW,  barH, texW, texH);
 
-        BufferUploader.drawWithShader(buf.end());
+        BufferUploader.drawWithShader(buf.buildOrThrow());
 
         //Ceremony name
         var name = cer.getDisplayName();
-        int nameX = (HUD_WIDTH - gui.getFont().width(name)) / 2;
-        guiGraphics.drawString(gui.getFont(), name, nameX, 2, 0xC8000000, false);
+        var font = Minecraft.getInstance().font;
+        int nameX = (HUD_WIDTH - font.width(name)) / 2;
+        guiGraphics.drawString(font, name, nameX, 2, 0xC8000000, false);
     }
 
-    private void renderCeremonyEffectHUD(StateCeremonyEffect state, ExtendedGui gui, GuiGraphics guiGraphics, float partialTick) {
+    @SuppressWarnings("resource")
+    private void renderCeremonyEffectHUD(StateCeremonyEffect state, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         final int texW = 128, texH = 64;
         final int barW = 104, barH = 7;
         var cer = state.getCeremony();
         var cerInst = state.getCeremonyInstance();
 
-        gui.setupOverlayRenderState(true, false);
         RenderSystem.setShaderTexture(0, CEREMONY_HUD_TEXTURE);
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        var buf = Tesselator.getInstance().getBuilder();
-        buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        var buf = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
         var poseStack = guiGraphics.pose();
 
         //Background
@@ -152,15 +152,17 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
         addQuad(buf, poseStack, 1, 20,  9, 9,   0, 48,  16, 16, texW, texH);
 
         //Time bar
+        float partialTick = deltaTracker.getGameTimeDeltaPartialTick(true);
         float timeW = Mth.clamp(1.0F - (state.getTime() + partialTick) / cerInst.getEffectTime(), 0.0F, 1.0F) * barW;
         addQuad(buf, poseStack, 11, 21,  timeW,  barH,  0, 32,  timeW,  barH, texW, texH);
 
-        BufferUploader.drawWithShader(buf.end());
+        BufferUploader.drawWithShader(buf.buildOrThrow());
 
         //Ceremony name
         var name = cer.getDisplayName();
-        int nameX = (HUD_WIDTH - gui.getFont().width(name)) / 2;
-        guiGraphics.drawString(gui.getFont(), name, nameX, 2, 0xC8000000, false);
+        var font = Minecraft.getInstance().font;
+        int nameX = (HUD_WIDTH - font.width(name)) / 2;
+        guiGraphics.drawString(font, name, nameX, 2, 0xC8000000, false);
     }
 
     //Like GuiGraphics.blit, but using the given BufferBuilder and allowing float values rather than int
@@ -168,9 +170,9 @@ public enum CeremonyHUD implements LayeredDraw.Layer {
         var mat = ps.last().pose();
         float minU = uOffset / textureWidth,  maxU = (uOffset + uWidth) / textureWidth;
         float minV = vOffset / textureHeight, maxV = (vOffset + vHeight) / textureHeight;
-        buf.vertex(mat, x,         y + height, 0).uv(minU, maxV).endVertex();
-        buf.vertex(mat, x + width, y + height, 0).uv(maxU, maxV).endVertex();
-        buf.vertex(mat, x + width, y,          0).uv(maxU, minV).endVertex();
-        buf.vertex(mat, x,         y,          0).uv(minU, minV).endVertex();
+        buf.addVertex(mat, x,         y + height, 0).setUv(minU, maxV);
+        buf.addVertex(mat, x + width, y + height, 0).setUv(maxU, maxV);
+        buf.addVertex(mat, x + width, y,          0).setUv(maxU, minV);
+        buf.addVertex(mat, x,         y,          0).setUv(minU, minV);
     }
 }
