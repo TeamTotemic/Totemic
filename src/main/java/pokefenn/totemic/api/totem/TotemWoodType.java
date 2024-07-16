407846eb76617da2a3dbe4e002df1b9d20982bb1
ljfa <ljfa-ag@users.noreply.github.com>
2024-07-16 11:04:43 +0200

    Add fallback to oak wood for wood type stream codec
    Move wood type codecs out of API

--- src/main/java/pokefenn/totemic/api/totem/TotemWoodType.java
+++ src/main/java/pokefenn/totemic/api/totem/TotemWoodType.java
@@ -2,11 +2,6 @@ package pokefenn.totemic.api.totem;
 
 import java.util.Objects;
 
-import com.mojang.serialization.Codec;
-
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
@@ -21,10 +16,6 @@ import pokefenn.totemic.api.TotemicAPI;
  *                  Any of the tagged blocks will be recognized as this wood type by the Totem Whittling Knife.
  */
 public record TotemWoodType(MapColor woodColor, MapColor barkColor, TagKey<Block> logTag) {
-    //TODO: How to make sure that this works when client and server are not synced?
-    public static final Codec<TotemWoodType> CODEC = TotemicAPI.get().registry().woodTypes().byNameCodec();
-    public static final StreamCodec<RegistryFriendlyByteBuf, TotemWoodType> STREAM_CODEC = ByteBufCodecs.fromCodecWithRegistries(CODEC);//ByteBufCodecs.registry(RegistryAPI.WOOD_TYPE_REGISTRY);
-
     public TotemWoodType {
         Objects.requireNonNull(woodColor);
         Objects.requireNonNull(barkColor);
--- src/main/java/pokefenn/totemic/init/ModDataComponents.java
+++ src/main/java/pokefenn/totemic/init/ModDataComponents.java
@@ -5,7 +5,9 @@ import java.util.function.Supplier;
 import com.mojang.serialization.Codec;
 
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.util.ExtraCodecs;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import pokefenn.totemic.api.TotemicAPI;
@@ -13,12 +15,18 @@ import pokefenn.totemic.api.totem.TotemCarving;
 import pokefenn.totemic.api.totem.TotemWoodType;
 
 public final class ModDataComponents {
+    /** Returns an error if the wood type is unknown. In case of items, this will log an error and change the wood type to oak. */
+    public static final Codec<TotemWoodType> WOOD_TYPE_CODEC = TotemicAPI.get().registry().woodTypes().byNameCodec();
+    /** Silently changes the wood type to oak if it is unknown. */
+    public static final Codec<TotemWoodType> WOOD_TYPE_CODEC_DEFAULTED = WOOD_TYPE_CODEC.orElseGet(ModContent.oak);
+    public static final StreamCodec<RegistryFriendlyByteBuf, TotemWoodType> WOOD_TYPE_STREAM_CODEC = ByteBufCodecs.fromCodecWithRegistries(WOOD_TYPE_CODEC_DEFAULTED);
+
     public static final DeferredRegister.DataComponents REGISTER = DeferredRegister.createDataComponents(TotemicAPI.MOD_ID);
 
     public static final Supplier<DataComponentType<TotemCarving>> CARVING = REGISTER.registerComponentType("carving", builder ->
             builder.persistent(TotemCarving.CODEC).networkSynchronized(TotemCarving.STREAM_CODEC).cacheEncoding());
     public static final Supplier<DataComponentType<TotemWoodType>> WOOD_TYPE = REGISTER.registerComponentType("wood_type", builder ->
-            builder.persistent(TotemWoodType.CODEC).networkSynchronized(TotemWoodType.STREAM_CODEC).cacheEncoding());
+            builder.persistent(WOOD_TYPE_CODEC).networkSynchronized(WOOD_TYPE_STREAM_CODEC).cacheEncoding());
     public static final Supplier<DataComponentType<Integer>> JINGLE_DRESS_CHARGE = REGISTER.registerComponentType("jd_charge", builder ->
             builder.networkSynchronized(ByteBufCodecs.VAR_INT)); //no need to save or sync this, but a StreamCodec is still required
     public static final Supplier<DataComponentType<Integer>> MEDICINE_BAG_CHARGE = REGISTER.registerComponentType("mb_charge", builder ->
