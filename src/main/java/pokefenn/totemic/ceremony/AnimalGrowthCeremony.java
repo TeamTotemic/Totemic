59e00449d6300c834e406d60943b56db1e0374b4
ljfa <ljfa-ag@users.noreply.github.com>
2024-08-28 20:51:50 +0200

    Refactor AnimalGrowthCeremony
    Give spawned chickens a random Y rotation

--- src/main/java/pokefenn/totemic/ceremony/AnimalGrowthCeremony.java
+++ src/main/java/pokefenn/totemic/ceremony/AnimalGrowthCeremony.java
@@ -8,13 +8,14 @@ import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Turtle;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.TurtleEggBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import pokefenn.totemic.api.TotemicAPI;
 import pokefenn.totemic.api.TotemicEntityUtil;
@@ -32,78 +33,83 @@ public enum AnimalGrowthCeremony implements CeremonyInstance {
     public void effect(Level level, BlockPos pos, CeremonyEffectContext context) {
         if(context.getTime() % 20 == 0) {
             var aabb = TotemicEntityUtil.getAABBAround(pos, RADIUS);
+            growAnimals(level, aabb);
+            hatchChickenEggs(level, aabb);
+            hatchTurtleEggs(level, pos);
+        }
+    }
 
-            //Animal growth
-            level.getEntitiesOfClass(Animal.class, aabb, Animal::isBaby)
-            .forEach(animal -> {
-                if(level.random.nextInt(4) == 0) {
-                    if(!level.isClientSide)
-                        growAnimal(level, animal);
-                    else {
-                        var animalBB = animal.getBoundingBox();
-                        spawnParticles(level, animal.position().add(0, animalBB.getYsize() * 0.5, 0), animalBB.getXsize() * 0.5, animalBB.getYsize() * 0.5, animalBB.getZsize() * 0.5);
-                    }
+    private static void growAnimals(Level level, AABB aabb) {
+        level.getEntitiesOfClass(Animal.class, aabb, Animal::isBaby)
+        .forEach(animal -> {
+            if(level.random.nextInt(4) == 0) {
+                if(!level.isClientSide) {
+                    animal.ageUp(level.random.nextInt(60));
+                    //the argument to ageUp is given in seconds,
+                    //this amounts to aging the animal up by about 337.5 s (out of 1200 s) each time the ceremony is used
                 }
-            });
-
-            //Chicken egg hatching
-            if(!level.isClientSide) {
-                level.getEntities(EntityType.ITEM, aabb, e -> e.getItem().is(Items.EGG))
-                .forEach(egg -> {
-                    if(level.random.nextInt(4) == 0)
-                        hatchChickenEgg(level, egg);
-                });
+                else {
+                    var animalBB = animal.getBoundingBox();
+                    spawnParticles(level, animal.position().add(0, animalBB.getYsize() * 0.5, 0), animalBB.getXsize() * 0.5, animalBB.getYsize() * 0.5, animalBB.getZsize() * 0.5);
+                }
             }
+        });
+    }
 
-            //Turtle egg hatching
-            TotemicAPI.get().ceremony().forEachBlockIn(level, TotemicEntityUtil.getBoundingBoxAround(pos, TURTLE_HATCH_RADIUS),
-            (p, state) -> {
-                if(state.is(Blocks.TURTLE_EGG) && TurtleEggBlock.onSand(level, p)) {
-                    if(!level.isClientSide) {
-                        if(level.random.nextInt(45) == 0) //about once per ceremony usage
-                            hatchTurtleEgg(level, p, state);
-                    }
-                    else {
-                        if(level.random.nextInt(4) == 0)
-                            spawnParticles(level, Vec3.atBottomCenterOf(p).add(0, 0.25, 0), 0.5, 0.5, 0.5);
-                    }
+    private static void hatchChickenEggs(Level level, AABB aabb) {
+        if(!level.isClientSide) {
+            level.getEntities(EntityType.ITEM, aabb, e -> e.getItem().is(Items.EGG))
+            .forEach(egg -> {
+                if(level.random.nextInt(4) == 0) {
+                    MiscUtil.spawnServerParticles(ParticleTypes.HAPPY_VILLAGER, level, egg.position(), 10, new Vec3(0.5, 0.5, 0.5), 1.0);
+                    var chicken = EntityType.CHICKEN.create(level);
+                    chicken.setAge(AgeableMob.BABY_START_AGE);
+                    chicken.moveTo(egg.position(), level.random.nextFloat() * 360.0F, 0.0F);
+                    level.addFreshEntity(chicken);
+                    MiscUtil.shrinkItemEntity(egg);
                 }
             });
         }
     }
 
-    private static void growAnimal(Level level, Animal animal) {
-        animal.ageUp(level.random.nextInt(60));
-        //the argument to ageUp is given in seconds,
-        //this amounts to aging the animal up by about 337.5 s (out of 1200 s) each time the ceremony is used
-    }
-
-    private static void hatchChickenEgg(Level level, ItemEntity egg) {
-        MiscUtil.spawnServerParticles(ParticleTypes.HAPPY_VILLAGER, level, egg.position(), 10, new Vec3(0.5, 0.5, 0.5), 1.0);
-        var chicken = EntityType.CHICKEN.create(level);
-        chicken.setPos(egg.position());
-        chicken.setAge(AgeableMob.BABY_START_AGE);
-        level.addFreshEntity(chicken);
-        MiscUtil.shrinkItemEntity(egg);
+    private static void hatchTurtleEggs(Level level, BlockPos pos) { //TODO: Look into how forEachBlockIn can be called less frequently
+        TotemicAPI.get().ceremony().forEachBlockIn(level, TotemicEntityUtil.getBoundingBoxAround(pos, TURTLE_HATCH_RADIUS),
+        (p, state) -> {
+            if(state.is(Blocks.TURTLE_EGG) && TurtleEggBlock.onSand(level, p)) {
+                if(!level.isClientSide) {
+                    if(level.random.nextInt(45) == 0) //about once per ceremony usage
+                        hatchTurtleEgg(level, p, state);
+                }
+                else {
+                    if(level.random.nextInt(4) == 0)
+                        spawnParticles(level, Vec3.atBottomCenterOf(p).add(0, 0.25, 0), 0.5, 0.5, 0.5);
+                }
+            }
+        });
     }
 
     private static void hatchTurtleEgg(Level level, BlockPos pos, BlockState state) {
+        //see TurtleEggBlock.randomTick
         int age = state.getValue(TurtleEggBlock.HATCH);
         if(age < 2) {
             level.playSound(null, pos, SoundEvents.TURTLE_EGG_CRACK, SoundSource.BLOCKS, 0.7F, 0.9F + level.random.nextFloat() * 0.2F);
             level.setBlock(pos, state.setValue(TurtleEggBlock.HATCH, age + 1), 2);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(state));
         }
         else {
             level.playSound(null, pos, SoundEvents.TURTLE_EGG_HATCH, SoundSource.BLOCKS, 0.7F, 0.9F + level.random.nextFloat() * 0.2F);
             level.removeBlock(pos, false);
+            level.gameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Context.of(state));
 
             for(int j = 0; j < state.getValue(TurtleEggBlock.EGGS); ++j) {
                 level.levelEvent(2001, pos, Block.getId(state));
                 Turtle turtle = EntityType.TURTLE.create(level);
-                turtle.setAge(AgeableMob.BABY_START_AGE);
-                turtle.setHomePos(pos);
-                turtle.moveTo(pos.getX() + 0.3D + j * 0.2D, pos.getY(), pos.getZ() + 0.3D, 0.0F, 0.0F);
-                level.addFreshEntity(turtle);
+                if(turtle != null) {
+                    turtle.setAge(AgeableMob.BABY_START_AGE);
+                    turtle.setHomePos(pos);
+                    turtle.moveTo(pos.getX() + 0.3 + j * 0.2, pos.getY(), pos.getZ() + 0.3, 0.0F, 0.0F);
+                    level.addFreshEntity(turtle);
+                }
             }
         }
     }
