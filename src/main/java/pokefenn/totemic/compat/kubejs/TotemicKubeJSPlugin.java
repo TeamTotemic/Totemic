833cca15c080d66d6de7121fb2b29360dc578bc6
ljfa <ljfa-ag@users.noreply.github.com>
2025-07-28 15:21:33 +0200

    Add binding for TotemicAPI and allow access to API package

--- src/main/java/pokefenn/totemic/compat/kubejs/TotemicKubeJSPlugin.java
+++ src/main/java/pokefenn/totemic/compat/kubejs/TotemicKubeJSPlugin.java
@@ -2,10 +2,17 @@ package pokefenn.totemic.compat.kubejs;
 
 import dev.latvian.mods.kubejs.event.EventGroupRegistry;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingRegistry;
+import pokefenn.totemic.api.TotemicAPI;
 
 public class TotemicKubeJSPlugin implements KubeJSPlugin {
     @Override
     public void registerEvents(EventGroupRegistry registry) {
         registry.register(TotemicKubeJSEvents.GROUP);
+    }
+
+    @Override
+    public void registerBindings(BindingRegistry bindings) {
+        bindings.add("TotemicAPI", TotemicAPI.get());
     }
 }
