8eebdc72c6f5dee37fca1ddf218239a94fe308ea
ljfa <ljfa-ag@users.noreply.github.com>
2025-09-04 20:24:19 +0200

    Replace build scripts with new ones using RetroFuturaGradle

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "Totemic-1.7.10"
+
+pluginManagement {
+  repositories {
+    maven {
+      // RetroFuturaGradle
+      name = "GTNH Maven"
+      url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+      mavenContent {
+        includeGroupByRegex("com\\.gtnewhorizons\\..+")
+        includeGroup("com.gtnewhorizons")
+      }
+    }
+    gradlePluginPortal()
+    mavenCentral()
+    mavenLocal()
+  }
+}
+
+plugins {
+  // Automatic toolchain provisioning
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
