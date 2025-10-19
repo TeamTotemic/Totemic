a0a184ea8c836ac38bfef36656a2a9b62a4e796c
ljfa <ljfa-ag@users.noreply.github.com>
2025-10-19 13:04:37 +0200

    Add instrument unlock advancements for locking Totempedia entries

--- src/main/java/pokefenn/totemic/Totemic.java
+++ src/main/java/pokefenn/totemic/Totemic.java
@@ -16,6 +16,7 @@ import pokefenn.totemic.api.TotemicAPI;
 import pokefenn.totemic.apiimpl.registry.RegistryApiImpl;
 import pokefenn.totemic.block.totem.entity.TotemBaseBlockEntity;
 import pokefenn.totemic.compat.kubejs.TotemicKubeEventHandler;
+import pokefenn.totemic.data.TotemicAdvancementProvider;
 import pokefenn.totemic.data.TotemicBlockStateProvider;
 import pokefenn.totemic.data.TotemicBlockTagsProvider;
 import pokefenn.totemic.data.TotemicDamageTypeTagsProvider;
@@ -101,6 +102,7 @@ public final class Totemic {
         gen.addProvider(event.includeServer(), new TotemicItemTagsProvider(out, lookup, blockTP.contentsGetter(), efh));
         gen.addProvider(event.includeServer(), new TotemicEntityTypeTagsProvider(out, lookup, efh));
         gen.addProvider(event.includeServer(), new TotemicLootTableProvider(out, lookup));
+        gen.addProvider(event.includeServer(), new TotemicAdvancementProvider(out, lookup, efh));
         gen.addProvider(event.includeServer(), new TotemicRecipeProvider(out, lookup));
         gen.addProvider(event.includeServer(), new TotemicDamageTypeTagsProvider(out, lookup, efh));
         gen.addProvider(event.includeServer(), new TotemicDataMapProvider(out, lookup));
---
+++ src/main/java/pokefenn/totemic/data/TotemicAdvancementProvider.java
@@ -0,0 +1,53 @@
+package pokefenn.totemic.data;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementRequirements.Strategy;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.AdvancementProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import pokefenn.totemic.Totemic;
+import pokefenn.totemic.advancements.criterion.CeremonyTrigger;
+import pokefenn.totemic.api.ceremony.Ceremony;
+import pokefenn.totemic.init.ModContent;
+import pokefenn.totemic.init.ModItems;
+
+public class TotemicAdvancementProvider extends AdvancementProvider {
+    public TotemicAdvancementProvider(PackOutput output, CompletableFuture<Provider> registries, ExistingFileHelper existingFileHelper) {
+        super(output, registries, existingFileHelper, List.of(TotemicAdvancementProvider::generate));
+    }
+
+    private static void generate(Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper efh) {
+        // Advacements for unlocking Totempedia Ceremony entries
+        var windChimeUnlocked = new Advancement.Builder()
+                .requirements(Strategy.OR)
+                .addCriterion("performed_fertility", performed(ModContent.fertility.get()))
+                .addCriterion("has_wind_chime", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.wind_chime))
+                .save(saver, Totemic.resloc("totempedia/wind_chime_unlocked"), efh);
+
+        var rattleUnlocked = new Advancement.Builder()
+                .parent(windChimeUnlocked)
+                .requirements(Strategy.OR)
+                .addCriterion("performed_buffalo_dance", performed(ModContent.buffalo_dance.get()))
+                .addCriterion("has_rattle", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.rattle))
+                .save(saver, Totemic.resloc("totempedia/rattle_unlocked"), efh);
+
+        new Advancement.Builder()
+                .parent(rattleUnlocked)
+                .requirements(Strategy.OR)
+                .addCriterion("performed_eagle_dance", performed(ModContent.eagle_dance.get()))
+                .addCriterion("has_eagle_bone_whistle", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.eagle_bone_whistle))
+                .save(saver, Totemic.resloc("totempedia/eagle_bone_whistle_unlocked"), efh);
+    }
+
+    private static Criterion<CeremonyTrigger.TriggerInstance> performed(Ceremony ceremony) {
+        return CeremonyTrigger.TriggerInstance.performedCeremony(ceremony);
+    }
+}
