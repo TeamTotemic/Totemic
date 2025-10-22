6e8f234bd1d94bd93d9cb78fda9830b650f5a161
ljfa <ljfa-ag@users.noreply.github.com>
2025-10-22 19:06:17 +0200

    Add missing KubeJS type wrappers

--- src/main/java/pokefenn/totemic/compat/kubejs/TotemicKubeJSPlugin.java
+++ src/main/java/pokefenn/totemic/compat/kubejs/TotemicKubeJSPlugin.java
@@ -3,6 +3,8 @@ package pokefenn.totemic.compat.kubejs;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.rhino.util.wrap.TypeWrappers;
 import pokefenn.totemic.api.TotemicAPI;
 import pokefenn.totemic.api.ceremony.Ceremony;
 import pokefenn.totemic.api.music.MusicInstrument;
@@ -30,5 +32,13 @@ public class TotemicKubeJSPlugin extends KubeJSPlugin {
     public void registerBindings(BindingsEvent bindings) {
         bindings.add("TotemicAPI", TotemicAPI.get());
         bindings.add("TotemEffect", TotemEffectWrapper.class);
+    }
+
+    @Override
+    public void registerTypeWrappers(ScriptType type, TypeWrappers typeWrappers) {
+        // Necessary in 1.20.1 for automatically wrapping strings
+        typeWrappers.register(MusicInstrument.class, MUSIC_INSTRUMENT);
+        typeWrappers.register(TotemCarving.class, TOTEM_CARVING);
+        typeWrappers.register(Ceremony.class, CEREMONY);
     }
 }
