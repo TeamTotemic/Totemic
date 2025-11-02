ace882e1d51c1a6e87cccc85380bf8e0efd8413e
ljfa <ljfa-ag@users.noreply.github.com>
2025-11-02 15:51:21 +0100

    Fix language entry generation for custom registered objects

--- src/main/java/pokefenn/totemic/compat/kubejs/TotemicRegistryKubeEvent.java
+++ src/main/java/pokefenn/totemic/compat/kubejs/TotemicRegistryKubeEvent.java
@@ -7,6 +7,7 @@ import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.event.StartupEventJS;
 import dev.latvian.mods.kubejs.registry.BuilderBase;
 import dev.latvian.mods.kubejs.registry.BuilderFactory;
+import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import dev.latvian.mods.kubejs.util.UtilsJS;
 import dev.latvian.mods.rhino.util.HideFromJS;
@@ -29,6 +30,7 @@ public class TotemicRegistryKubeEvent<T> extends StartupEventJS {
         var resLoc = UtilsJS.getMCID(ScriptType.STARTUP.manager.get().context, KubeJS.appendModId(id));
         var builder = builderFactory.createBuilder(resLoc);
         builders.add(builder);
+        RegistryInfo.ALL_BUILDERS.add(builder); // for generating language entries, hopefully this doesn't cause any problems
         return builder;
     }
 
