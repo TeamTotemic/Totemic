d23902d383c15c83d4579c48100e34814782df5a
ljfa <ljfa-ag@users.noreply.github.com>
2026-01-28 02:16:12 +0100

    Use AT instead of Reflection for Ocelot effect
    Remove random component of Ocelot effect

--- src/main/java/totemic_commons/pokefenn/totem/TotemEffectOcelot.java
+++ src/main/java/totemic_commons/pokefenn/totem/TotemEffectOcelot.java
@@ -1,11 +1,5 @@
 package totemic_commons.pokefenn.totem;
 
-import static totemic_commons.pokefenn.Totemic.logger;
-
-import java.lang.reflect.Field;
-import java.util.Random;
-
-import cpw.mods.fml.relauncher.ReflectionHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.world.World;
@@ -25,42 +19,23 @@ public class TotemEffectOcelot extends TotemEffect
         super(modid, baseName, horizontal, vertical, tier);
     }
 
-    private final Field timeSinceIgnited = ReflectionHelper.findField(EntityCreeper.class, "timeSinceIgnited", "field_70833_d", "bq");
-
     @Override
     public void effect(World world, int x, int y, int z, int poleSize, int horizontal, int vertical, int melodyAmount, int totemWoodBonus, int repetitionBonus)
     {
         if(world.isRemote)
             return;
 
-        try
+        for(Entity entity : EntityUtil.getEntitiesInRange(world, x, y, z, horizontal, vertical))
         {
-            for(Entity entity : EntityUtil.getEntitiesInRange(world, x, y, z, horizontal, vertical))
+            if(entity instanceof EntityCreeper)
             {
-                if(entity instanceof EntityCreeper)
+                EntityCreeper creeper = (EntityCreeper)entity;
+                if(creeper.timeSinceIgnited > 20 - repetitionBonus)
                 {
-                    int ignited = (Integer) timeSinceIgnited.get(entity);
-
-                    if(repetitionBonus < 5)
-                    {
-                        Random random = new Random();
-                        if(random.nextInt(4 + repetitionBonus + (melodyAmount / 16)) == 1)
-                        {
-                            return;
-                        }
-                    }
-
-                    if(ignited > 20 - repetitionBonus)
-                    {
-                        timeSinceIgnited.setInt(entity, 0);
-                        ((EntityCreeper)entity).setCreeperState(-1);
-                    }
+                    creeper.timeSinceIgnited = 0;
+                    creeper.setCreeperState(-1);
                 }
             }
-        }
-        catch(IllegalAccessException e)
-        {
-            logger.error("Could not perform Ocelot Totem effect", e);
         }
     }
 
