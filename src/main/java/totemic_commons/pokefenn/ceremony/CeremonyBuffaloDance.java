05057ad06d317b76d5404c72ea8ab58b6975dd0e
ljfa <ljfa-ag@users.noreply.github.com>
2026-02-23 00:31:38 +0100

    Fix Buffalo Dance not preserving leashes

--- src/main/java/totemic_commons/pokefenn/ceremony/CeremonyBuffaloDance.java
+++ src/main/java/totemic_commons/pokefenn/ceremony/CeremonyBuffaloDance.java
@@ -1,7 +1,7 @@
 package totemic_commons.pokefenn.ceremony;
 
 import net.minecraft.command.IEntitySelector;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
@@ -31,7 +31,7 @@ public class CeremonyBuffaloDance extends Ceremony
 
         int buffalos = 0;
         AxisAlignedBB aabb = AxisAlignedBB.getBoundingBox(x - 0.5, y - 0.5, z - 0.5, x + 0.5, y + 0.5, z + 0.5).expand(8, 8, 8);
-        for(EntityLivingBase entity : world.selectEntitiesWithinAABB(EntityLivingBase.class, aabb, selector))
+        for(EntityLiving entity : world.selectEntitiesWithinAABB(EntityLiving.class, aabb, selector))
         {
             if(buffalos < 2)
             {
@@ -40,6 +40,8 @@ public class CeremonyBuffaloDance extends Ceremony
                 float health = entity.getHealth() / entity.getMaxHealth() * buffalo.getMaxHealth();
                 buffalo.setHealth(health);
                 EntityUtil.spawnEntity(world, entity.posX, entity.posY, entity.posZ, buffalo);
+                if(entity.getLeashed())
+                    buffalo.setLeashedToEntity(entity.getLeashedToEntity(), true);
                 entity.setDead();
             }
         }
