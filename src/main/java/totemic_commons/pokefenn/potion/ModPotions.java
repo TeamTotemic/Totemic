399161f92f63eaec72da3a44a5bcaee15ec2d98d
ljfa <ljfa-ag@users.noreply.github.com>
2026-05-17 01:22:23 +0200

    Add potion icons

--- src/main/java/totemic_commons/pokefenn/potion/ModPotions.java
+++ src/main/java/totemic_commons/pokefenn/potion/ModPotions.java
@@ -11,15 +11,14 @@ import totemic_commons.pokefenn.configuration.ConfigurationSettings;
  */
 public class ModPotions
 {
-
     public static Potion batPotion;
     public static Potion spiderPotion;
     public static Potion ocelotPotion;
 
     public static void init()
     {
-        batPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_BAT, false, 0xF2F2F0).setPotionName("Bat");
-        spiderPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_SPIDER, false, 0x524354).setPotionName("Spider");
-        ocelotPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_OCELOT, false, 0x52432D).setPotionName("Ocelot");
+        batPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_BAT, false, 0xF2F2F0, "bat.png").setPotionName("Bat");
+        spiderPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_SPIDER, false, 0x524354, "spider.png").setPotionName("Spider");
+        ocelotPotion = new PotionTotemic(ConfigurationSettings.POTION_ID_OCELOT, false, 0x52432D, "ocelot.png").setPotionName("Ocelot");
     }
 }
--- src/main/java/totemic_commons/pokefenn/potion/PotionTotemic.java
+++ src/main/java/totemic_commons/pokefenn/potion/PotionTotemic.java
@@ -1,11 +1,39 @@
 package totemic_commons.pokefenn.potion;
 
+import javax.annotation.Nullable;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 
 public class PotionTotemic extends Potion
 {
+    private final @Nullable ResourceLocation texture;
+
+    public PotionTotemic(int id, boolean isBadEffect, int color, String textureName)
+    {
+        super(id, isBadEffect, color);
+        this.texture = new ResourceLocation("totemic", "textures/potions/" + textureName);
+    }
+
     public PotionTotemic(int id, boolean isBadEffect, int color)
     {
         super(id, isBadEffect, color);
+        this.texture = null;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc)
+    {
+        if(texture != null)
+        {
+            mc.getTextureManager().bindTexture(texture);
+            Gui.func_146110_a(x + 6, y + 7, 0.0f, 0.0f, 18, 18, 18f, 18f);
+        }
     }
 }
