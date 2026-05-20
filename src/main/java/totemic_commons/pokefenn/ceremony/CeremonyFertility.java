b726abe4fce0d0431ab7f5c3427eeb79f4f3c2fe
ljfa <ljfa-ag@users.noreply.github.com>
2026-05-21 01:52:46 +0200

    Implement villager mating

--- src/main/java/totemic_commons/pokefenn/ceremony/CeremonyFertility.java
+++ src/main/java/totemic_commons/pokefenn/ceremony/CeremonyFertility.java
@@ -9,8 +9,8 @@ import java.util.function.Predicate;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityVillager;
@@ -81,7 +81,9 @@ public class CeremonyFertility extends Ceremony
                 EntityVillager villager = (EntityVillager) entity;
                 if(villager.getGrowingAge() == 0 && !matedVillagers.contains(villager) && !villager.isMating() && consumeBreedingItem(world, aabb, villager))
                 {
-                    // TODO Set villager in love
+                    matedVillagers.add(villager);
+                    villager.tasks.addTask(0, new EntityAIVillagerFertility(villager));
+                    world.setEntityState(villager, (byte) 12); //Spawns heart particles
                     return; // Limit to one animal or villager per second
                 }
             }
@@ -117,5 +119,96 @@ public class CeremonyFertility extends Ceremony
     {
         List<EntityItem> list = world.selectEntitiesWithinAABB(EntityItem.class, aabb, entity -> predicate.test(((EntityItem) entity).getEntityItem()));
         return !list.isEmpty() ? Optional.of(list.get(0)) : Optional.empty();
+    }
+
+    // A replacement for EntityAIVillagerMate that makes villagers mate regardless of the number of doors in a village, etc.
+    private static class EntityAIVillagerFertility extends EntityAIBase
+    {
+        private final EntityVillager villager;
+        private EntityVillager mate;
+        private final World world;
+        private int matingTimeout;
+
+        EntityAIVillagerFertility(EntityVillager villager)
+        {
+            this.villager = villager;
+            this.world = villager.worldObj;
+            this.setMutexBits(3);
+        }
+        
+        @Override
+        public boolean shouldExecute()
+        {
+            if(this.villager.getGrowingAge() == 0)
+            {
+                EntityVillager mate = (EntityVillager) this.world.findNearestEntityWithinAABB(EntityVillager.class, this.villager.boundingBox.expand(8.0D, 3.0D, 8.0D), this.villager);
+                if (mate == null)
+                    return false;
+                else
+                {
+                    this.mate = mate;
+                    return mate.getGrowingAge() == 0 && matedVillagers.contains(mate);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        @Override
+        public void startExecuting()
+        {
+            this.matingTimeout = 300;
+            this.villager.setMating(true);
+        }
+
+        @Override
+        public void resetTask()
+        {
+            this.mate = null;
+            this.villager.setMating(false);
+            matedVillagers.remove(this.villager);
+            matedVillagers.remove(this.mate);
+            this.villager.tasks.removeTask(this); // FIXME: this causes infinite recursion, can't do it like that
+        }
+
+        @Override
+        public boolean continueExecuting()
+        {
+            return this.matingTimeout >= 0 && this.villager.getGrowingAge() == 0 && matedVillagers.contains(mate);
+        }
+
+        @Override
+        public void updateTask()
+        {
+            --this.matingTimeout;
+            this.villager.getLookHelper().setLookPositionWithEntity(this.mate, 10.0F, 30.0F);
+
+            if (this.villager.getDistanceSqToEntity(this.mate) > 2.25D)
+            {
+                this.villager.getNavigator().tryMoveToEntityLiving(this.mate, 0.25D);
+            }
+            else if (this.matingTimeout == 0 && this.mate.isMating())
+            {
+                this.giveBirth();
+            }
+
+            if (this.villager.getRNG().nextInt(35) == 0)
+            {
+                this.world.setEntityState(this.villager, (byte)12);
+            }
+        }
+
+        private void giveBirth()
+        {
+            EntityVillager entityvillager = this.villager.createChild(this.mate);
+            this.mate.setGrowingAge(6000);
+            this.villager.setGrowingAge(6000);
+            entityvillager.setGrowingAge(-24000);
+            entityvillager.setLocationAndAngles(this.villager.posX, this.villager.posY, this.villager.posZ, 0.0F, 0.0F);
+            this.world.spawnEntityInWorld(entityvillager);
+            this.world.setEntityState(entityvillager, (byte)12);
+        }
     }
 }
