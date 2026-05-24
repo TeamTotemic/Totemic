b5a93ac6398a1c46abee254a9c90b414c10b7e3f
ljfa <ljfa-ag@users.noreply.github.com>
2026-05-24 18:34:39 +0200

    New approach for villager mating

--- src/main/java/totemic_commons/pokefenn/ceremony/CeremonyFertility.java
+++ src/main/java/totemic_commons/pokefenn/ceremony/CeremonyFertility.java
@@ -1,14 +1,14 @@
 package totemic_commons.pokefenn.ceremony;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.WeakHashMap;
 import java.util.function.Predicate;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.item.EntityItem;
@@ -26,8 +26,8 @@ import totemic_commons.pokefenn.util.TotemUtil;
 
 public class CeremonyFertility extends Ceremony
 {
-    //Weak set to keep track which villagers have been affected by the ceremony
-    private static final Set<EntityVillager> matedVillagers = Collections.newSetFromMap(new WeakHashMap<>());
+    // Weak map to keep track which villagers have been affected by the ceremony
+    private static final Map<EntityVillager, EntityAIVillagerFertility> villagerFertilityTasks = new WeakHashMap<>();
 
     public CeremonyFertility(String modid, String name, int musicNeeded, int maxStartupTime, int effectTime, int musicPer5, MusicInstrument... instruments)
     {
@@ -79,18 +79,35 @@ public class CeremonyFertility extends Ceremony
             else if(entity instanceof EntityVillager)
             {
                 EntityVillager villager = (EntityVillager) entity;
-                if(villager.getGrowingAge() == 0 && !matedVillagers.contains(villager) && !villager.isMating() && consumeBreedingItem(world, aabb, villager))
+                if(villager.getGrowingAge() == 0 && !villager.isMating())
                 {
-                    matedVillagers.add(villager);
-                    villager.tasks.addTask(0, new EntityAIVillagerFertility(villager));
-                    world.setEntityState(villager, (byte) 12); //Spawns heart particles
-                    return; // Limit to one animal or villager per second
+                    // Add Fertility AI task if the villager does not already have it.
+                    //
+                    // Since a task cannot safely remove itself in 1.7.10 (leads to inifite recursion or
+                    // ConcurrentModificationException), the villagers will keep the task indefinitely but have it only
+                    // activate when this Ceremony is performed.
+                    // We use the weak map to avoid adding the task to the same villager multiple times and look it up
+                    // if it's already added.
+                    EntityAIVillagerFertility task = villagerFertilityTasks.get(villager);
+                    if(task == null)
+                    {
+                        task = new EntityAIVillagerFertility(villager);
+                        villager.tasks.addTask(1, task);
+                        villagerFertilityTasks.put(villager, task);
+                    }
+
+                    if(!task.isActive() && consumeBreedingItem(world, aabb, villager))
+                    {
+                        task.setActive(true);
+                        world.setEntityState(villager, (byte) 12); //Spawns heart particles
+                        return; // Limit to one animal or villager per second
+                    }
                 }
             }
         }
     }
 
-    private boolean consumeBreedingItem(World world, AxisAlignedBB aabb, EntityAnimal animal)
+    private static boolean consumeBreedingItem(World world, AxisAlignedBB aabb, EntityAnimal animal)
     {
         Optional<EntityItem> itemE = findItemEntity(world, aabb, animal::isBreedingItem);
         if(itemE.isPresent())
@@ -103,7 +120,7 @@ public class CeremonyFertility extends Ceremony
             return false;
     }
 
-    private boolean consumeBreedingItem(World world, AxisAlignedBB aabb, EntityVillager villager)
+    private static boolean consumeBreedingItem(World world, AxisAlignedBB aabb, EntityVillager villager)
     {
         Optional<EntityItem> itemE = findItemEntity(world, aabb, item -> item.getItem() == Items.emerald);
         if(itemE.isPresent())
@@ -121,38 +138,55 @@ public class CeremonyFertility extends Ceremony
         return !list.isEmpty() ? Optional.of(list.get(0)) : Optional.empty();
     }
 
-    // A replacement for EntityAIVillagerMate that makes villagers mate regardless of the number of doors in a village, etc.
+    // A copy of EntityAIVillagerMate that makes villagers mate if active, regardless of the number of doors in a village, etc.
     private static class EntityAIVillagerFertility extends EntityAIBase
     {
-        private final EntityVillager villager;
+        private EntityVillager villagerObj;
         private EntityVillager mate;
-        private final World world;
+        private World worldObj;
         private int matingTimeout;
+
+        // The task will only run when this is set to true, which is done by the Fertility ceremony.
+        private boolean isActive = false;
 
         EntityAIVillagerFertility(EntityVillager villager)
         {
-            this.villager = villager;
-            this.world = villager.worldObj;
+            this.villagerObj = villager;
+            this.worldObj = villager.worldObj;
             this.setMutexBits(3);
+        }
+
+        boolean isActive()
+        {
+            return isActive;
+        }
+
+        void setActive(boolean isActive)
+        {
+            this.isActive = isActive;
         }
         
         @Override
         public boolean shouldExecute()
         {
-            if(this.villager.getGrowingAge() == 0)
+            if (!this.isActive || this.villagerObj.getGrowingAge() != 0)
             {
-                EntityVillager mate = (EntityVillager) this.world.findNearestEntityWithinAABB(EntityVillager.class, this.villager.boundingBox.expand(8.0D, 3.0D, 8.0D), this.villager);
-                if (mate == null)
-                    return false;
-                else
-                {
-                    this.mate = mate;
-                    return mate.getGrowingAge() == 0 && matedVillagers.contains(mate);
-                }
+                return false;
             }
             else
             {
-                return false;
+                // TODO: This will always pick the closest nearby villager, even if they aren't able to mate. Not sure if this is problematic.
+                Entity mate = this.worldObj.findNearestEntityWithinAABB(EntityVillager.class, this.villagerObj.boundingBox.expand(8.0D, 3.0D, 8.0D), this.villagerObj);
+
+                if (mate == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    this.mate = (EntityVillager)mate;
+                    return this.mate.getGrowingAge() == 0;
+                }
             }
         }
 
@@ -160,55 +194,53 @@ public class CeremonyFertility extends Ceremony
         public void startExecuting()
         {
             this.matingTimeout = 300;
-            this.villager.setMating(true);
+            this.villagerObj.setMating(true);
         }
 
         @Override
         public void resetTask()
         {
+            this.isActive = false;
             this.mate = null;
-            this.villager.setMating(false);
-            matedVillagers.remove(this.villager);
-            matedVillagers.remove(this.mate);
-            this.villager.tasks.removeTask(this); // FIXME: this causes infinite recursion, can't do it like that
+            this.villagerObj.setMating(false);
         }
 
         @Override
         public boolean continueExecuting()
         {
-            return this.matingTimeout >= 0 && this.villager.getGrowingAge() == 0 && matedVillagers.contains(mate);
+            return this.isActive && this.matingTimeout >= 0 && this.villagerObj.getGrowingAge() == 0;
         }
 
         @Override
         public void updateTask()
         {
             --this.matingTimeout;
-            this.villager.getLookHelper().setLookPositionWithEntity(this.mate, 10.0F, 30.0F);
-
-            if (this.villager.getDistanceSqToEntity(this.mate) > 2.25D)
+            this.villagerObj.getLookHelper().setLookPositionWithEntity(this.mate, 10.0F, 30.0F);
+    
+            if (this.villagerObj.getDistanceSqToEntity(this.mate) > 2.25D)
             {
-                this.villager.getNavigator().tryMoveToEntityLiving(this.mate, 0.25D);
+                this.villagerObj.getNavigator().tryMoveToEntityLiving(this.mate, 0.25D);
             }
             else if (this.matingTimeout == 0 && this.mate.isMating())
             {
                 this.giveBirth();
             }
-
-            if (this.villager.getRNG().nextInt(35) == 0)
+    
+            if (this.villagerObj.getRNG().nextInt(35) == 0)
             {
-                this.world.setEntityState(this.villager, (byte)12);
+                this.worldObj.setEntityState(this.villagerObj, (byte)12);
             }
         }
 
         private void giveBirth()
         {
-            EntityVillager entityvillager = this.villager.createChild(this.mate);
+            EntityVillager entityvillager = this.villagerObj.createChild(this.mate);
             this.mate.setGrowingAge(6000);
-            this.villager.setGrowingAge(6000);
+            this.villagerObj.setGrowingAge(6000);
             entityvillager.setGrowingAge(-24000);
-            entityvillager.setLocationAndAngles(this.villager.posX, this.villager.posY, this.villager.posZ, 0.0F, 0.0F);
-            this.world.spawnEntityInWorld(entityvillager);
-            this.world.setEntityState(entityvillager, (byte)12);
+            entityvillager.setLocationAndAngles(this.villagerObj.posX, this.villagerObj.posY, this.villagerObj.posZ, 0.0F, 0.0F);
+            this.worldObj.spawnEntityInWorld(entityvillager);
+            this.worldObj.setEntityState(entityvillager, (byte)12);
         }
     }
 }
