7688bed83e957b7db6d77adbcd86761247b45f19
ljfa <ljfa-ag@users.noreply.github.com>
2026-06-04 00:00:32 +0200

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 // Project properties
 group = "totemic_commons.pokefenn"
-version = "1.7.10-0.6.2"
+version = "1.7.10-0.7.0"
 base.archivesName = "Totemic"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
